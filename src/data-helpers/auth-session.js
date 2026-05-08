6ce6cd9c6f4b900f9aba1fdd777edd4bd7f05888
Dev Lewa <omolewastephen@gmail.com>
2026-05-09 00:47:02 +0100

    fix: add SameSite, Secure flags and encode cookie values in auth-session

--- src/data-helpers/auth-session.js
+++ src/data-helpers/auth-session.js
@@ -1,38 +1,48 @@
-// Created by: Aremu Olayinka Abayomi
-// Helper site: https://www.tabnine.com/academy/javascript/how-to-set-cookies-javascript/
+const getCookieAttributes = (expDays) => {
+  const attributes = ['path=/', 'SameSite=Lax'];
+
+  if (Number.isFinite(expDays)) {
+    const date = new Date();
+    date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000);
+    attributes.push(`expires=${date.toUTCString()}`);
+  }
+
+  if (typeof window !== 'undefined' && window.location.protocol === 'https:') {
+    attributes.push('Secure');
+  }
+
+  return attributes.join('; ');
+};
 
 export function storeDataInCookie(cName, cValue, expDays) {
-  if (typeof window !== "undefined") {
-    let date = new Date();
-    date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000);
-    const expires = "expires=" + date.toUTCString();
-    window.document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
+  if (typeof window !== 'undefined' && cName && cValue != null) {
+    window.document.cookie = `${cName}=${encodeURIComponent(cValue)}; ${getCookieAttributes(expDays)}`;
   }
 }
 
 export function getDataInCookie(cName) {
-  if (typeof window !== "undefined") {
-    const name = cName + "=";
-    const cDecoded = decodeURIComponent(window.document.cookie); //to be careful
-    const cArr = cDecoded.split("; ");
+  if (typeof window !== 'undefined') {
+    const name = cName + '=';
+    const cArr = window.document.cookie.split('; ');
     let res;
     cArr.forEach((val) => {
-      if (val.indexOf(name) === 0) res = val.substring(name.length);
+      if (val.indexOf(name) === 0) {
+        res = decodeURIComponent(val.substring(name.length));
+      }
     });
     return res;
   }
 }
 
 export function deleteDataInCookie(cName) {
-  if (typeof window !== "undefined") {
-    document.cookie = `${cName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
+  if (typeof window !== 'undefined') {
+    const secure = window.location.protocol === 'https:' ? '; Secure' : '';
+    document.cookie = `${cName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Lax${secure}`;
   }
 }
 
 export function deleteAllCookie() {
-  if (typeof window !== "undefined") {
-    document.cookie = "_id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
-    document.cookie =
-      "access_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
-  }
+  deleteDataInCookie('_id');
+  deleteDataInCookie('access_token__admin');
+  deleteDataInCookie('refresh_token__admin');
 }
